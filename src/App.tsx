1137eedf5b9a5cd40f6e736bf33ad2a0e73e9c61
daniel57060 <a57060@alunos.ipb.pt>
2023-04-22 16:05:30 +0100

    Copy dashboard template

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
-import { Box, Container, Typography } from '@mui/material'
-import Copyright from './Copyright'
-import ProTip from './ProTip'
+import { MainPage } from './pages/main'
 
 // https://github.com/mui/material-ui/blob/master/examples/material-vite/src/App.jsx
 export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Vite.js example
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  )
+  return <MainPage></MainPage>
 }
