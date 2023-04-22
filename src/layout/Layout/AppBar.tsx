8a03c750658a75f1f5131a61fc1d28d5d918a1f9
daniel57060 <a57060@alunos.ipb.pt>
2023-04-22 20:56:46 +0100

    Add avatar to Layout/AppBar

--- src/layout/Layout/AppBar.tsx
+++ src/layout/Layout/AppBar.tsx
@@ -1,5 +1,8 @@
-import PersonIcon from '@mui/icons-material/Person'
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import Avatar from '@mui/material/Avatar'
+import Box, { BoxProps } from '@mui/material/Box'
+import Badge from '@mui/material/Badge'
 import IconButton from '@mui/material/IconButton'
 import { styled } from '@mui/material/styles'
 import Toolbar from '@mui/material/Toolbar'
@@ -11,9 +14,11 @@ interface AppBarProps extends MuiAppBarProps {
   open?: boolean
 }
 
-const AppBar = styled(MuiAppBar, {
+const AppBarHeader = (props: BoxProps) => <Box component="header" {...props} />
+const AppBar = styled(AppBarHeader, {
   shouldForwardProp: (prop) => prop !== 'open',
 })<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
   ...(open
     ? {
         marginLeft: drawerWidthOpen,
@@ -33,7 +38,11 @@ const AppBar = styled(MuiAppBar, {
       }),
 }))
 
-export default function MainAppBar({ open }: { open: boolean }) {
+type MainAppBarProps = {
+  open: boolean
+}
+
+export default function MainAppBar({ open }: MainAppBarProps) {
   return (
     <AppBar position="absolute" open={open}>
       <Toolbar
@@ -41,11 +50,17 @@ export default function MainAppBar({ open }: { open: boolean }) {
           pr: '24px', // keep right padding when drawer closed
         }}
       >
-        <Typography component="h1" variant="h6" color="inherit" noWrap sx={{ flexGrow: 1 }}>
-          Dashboard
+        <Typography component="h2" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          Welcome Lorena Kasper
         </Typography>
         <IconButton color="inherit">
-          <PersonIcon />
+          <Badge badgeContent={4} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <Box pr={2} />
+        <IconButton color="inherit">
+          <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/3.jpg" sx={{ width: 56, height: 56 }} />
         </IconButton>
       </Toolbar>
     </AppBar>
