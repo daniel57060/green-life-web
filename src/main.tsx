e6ed69e12da66d4e4bb9302cf8dea4b9b2d104f8
daniel57060 <a57060@alunos.ipb.pt>
2023-04-22 15:49:56 +0100

    Add material ui dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@emotion/react'
+import theme from './theme.ts'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
